c9cb18eb4e402b59f14103cdeb0ed24363b37fcf
Harry Harpham <harry.harpham@evariant.com>
2016-05-16 00:22:58 -0500

    Add skeleton of log tests

---
+++ log_test.go
@@ -0,0 +1,31 @@
+package persisted
+
+import "testing"
+
+func TestNewLog(t *testing.T) {
+	// TODO: implement me!
+}
+
+func TestAdd(t *testing.T) {
+	// TODO: implement me!
+}
+
+func TestReplay(t *testing.T) {
+	// TODO: implement me!
+}
+
+func TestCompact(t *testing.T) {
+	// TODO: implement me!
+}
+
+func TestCompactIfNecessary(t *testing.T) {
+	// TODO: implement me!
+}
+
+func TestStateChangeMarshal(t *testing.T) {
+	// TODO: implement me!
+}
+
+func TestStateChangeUnmarshal(t *testing.T) {
+	// TODO: implement me!
+}
